efade61d29f3555015ff6b8ee590e68581c6bc5b
ZooWee <condreaychris@gmail.com>
2021-10-29 15:22:09 -0700

    js complete saved local storage

--- Main/assets/js/script.js
+++ Main/assets/js/script.js
@@ -1,6 +1,30 @@
-var today = new Date();
-var date = today.getFullYear() + '-' + (today.getMonth()+1) + '-' + today.getDate();
+var today = moment("11/3/20", "MM-DD-YY").format("dddd, MMMM Do YYYY");
+$("#currentDay").text(today);
 
-document.getElementById('currentDay').innerHTML = date;
+var buttonArr = document.querySelectorAll('.saveBtn');
 
-const button = ()
+// button.addEventListener('click', function() {
+//   button.previousElementSibling;
+//   console.log(button.previousElementSibling)
+// })
+
+
+buttonArr.forEach(function(element, index) {
+  element.previousElementSibling.value = localStorage.getItem("time" + index) || ""
+
+  element.addEventListener('click', function() {
+    localStorage.setItem("time" + index, element.previousElementSibling.value);
+    console.log(element.previousElementSibling.value)
+  })
+  console.log(element)
+})
+
+// falsey: 0, "", null, false, "undefined", NaN, 0n, -0
+function checkTime(textArea) {
+  var currentHour = moment().format("H")
+  if (textArea == currentHour) {
+    
+  }
+}
+
+console.log(moment().format("H")); // 9 === hourBlockId
