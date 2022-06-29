2083237f1c9227b96ddc7abc93f6da5734c7f75d
The Instigator <condreaychris@gmail.com>
2022-06-29 04:30:08 -0700

    time color and button save

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,50 @@
-var buttonArr = $('.saveBtn');
+var containerEl = $('.container')
+var saveButton = $('.save-button');
 var textArea = $('.description');
 
 var today = moment()
  $('#currentDay').text(today.format('MMMM Do, YYYY'))
 
+savedDescription();
 
-// buttonArr.forEach(function(element, index) {
-//   element.previousElementSibling.value = localStorage.getItem("time" + index) || ""
+ function savedDescription() {
+  var description = localStorage.getItem("description");
 
-//   element.addEventListener('click', function() {
-//     localStorage.setItem("time" + index, element.previousElementSibling.value);
-//     console.log(element.previousElementSibling.value)
-//   })
-//   console.log(element)
-// })
+  textArea.text(description);
+ }
 
 function checkTime() {
-  var currentHour = moment().format("LT")
-  console.log(currentHour)
-  if (textArea === currentHour) {
-    textArea.addCLass('.present');
-  }
+  var currentTime = today.hours();
+  var timeBlockEl = $('.time-block');
+
+  timeBlockEl.each(function() {
+      var dataTime = parseInt($(this).attr('data-time').split('-')[1]);
+
+    if (dataTime < currentTime) {
+      $(this).addClass('past');
+    }
+    else if (dataTime === currentTime) {
+      $(this).removeClass('past');
+      $(this).addClass('present');
+    } 
+    else {
+      $(this).removeClass('past');
+      $(this).addClass('future');
+    }
+  });
 }
 
 checkTime()
+
+saveButton.on('click', function(event) {
+  event.preventDefault()
+
+  var description = $('.description').val()
+
+  if (description === "") {
+    return
+  } else {
+    localStorage.setItem("description", description);
+    savedDescription();
+  }
+});
